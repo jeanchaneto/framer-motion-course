5d8ec8fd8ba0f1a3aee91868a3268ca8d144cd26
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-09 19:52:19 +0200

    Hover animations

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -8,18 +8,21 @@ const Base = ({ addBase, pizza }) => {
   return (
     <motion.div
       className="base container"
-      initial={{ x: '+100vw' }}
+      initial={{ x: "+100vw" }}
       animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}
+      transition={{ type: "spring", delay: 0.5 }}
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map((base) => {
           let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li key={base} onClick={() => addBase(base)}
+            whileHover={{scale: 1.3, originX: 0, color: '#f8e112'}}
+            transition={{type: 'spring', stiffness:300}}
+            >
               <span className={spanClass}>{base}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
@@ -32,7 +35,15 @@ const Base = ({ addBase, pizza }) => {
           transition={{ type: "spring", stiffness: 120 }}
         >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: "0px 0px 8px rgb(255,255,255",
+                textShadow: "0px 0px 8px rgb(255,255,255",
+              }}
+            >
+              Next
+            </motion.button>
           </Link>
         </motion.div>
       )}
