7987489ea319ee8527265dec564bdf2a986edb15
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-11 20:00:55 +0200

    Cycle animations with useCycle hook

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, useCycle } from 'framer-motion'
 
 const loaderVariants = {
     animationOne: {
@@ -18,19 +18,32 @@ const loaderVariants = {
                 duration: 0.25
              }
         }
+    },
+    animationTwo: {
+        y: [0, 40],
+        x: 0,
+        transition: {
+            y: {
+                ease: "easeOut",
+                repeatType: "reverse",
+                repeat: Infinity,
+                duration: 0.25
+             }
+        }
     }
 }
 
 const Loader = () => {
+const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+
   return (
     <>
     <motion.div className="loader" 
     variants={loaderVariants}
-    animate= "animationOne"
+    animate= {animation}
     >
-    
-
     </motion.div>
+    <div onClick={() =>cycleAnimation()} >Cycle Animation</div>
     </>
   )
 }
