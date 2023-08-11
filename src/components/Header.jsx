9dca28cc24d93c713a2e05c880d8c6287aa265e5
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-08-11 20:26:37 +0200

    Drag animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,34 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const svgVariant = {
-  hidden: {rotate: -180},
-  visible : {
+  hidden: { rotate: -180 },
+  visible: {
     rotate: 0,
-    transition: {duration: 1}
-  }
-}
+    transition: { duration: 1 },
+  },
+};
 
 const pathVariants = {
   hidden: {
     opacity: 0,
-    pathLength: 0
+    pathLength: 0,
   },
   visible: {
     opacity: 1,
     pathLength: 1,
-    transition: {duration : 2, ease: "easeInOut"}
-  }
-}
+    transition: { duration: 2, ease: "easeInOut" },
+  },
+};
 
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={0.75}
+      >
         <motion.svg
           className="pizza-svg"
           variants={svgVariant}
@@ -34,20 +39,23 @@ const Header = () => {
           viewBox="0 0 100 100"
         >
           <motion.path
-          variants={pathVariants}
+            variants={pathVariants}
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
-          <motion.path variants={pathVariants} fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
+          <motion.path
+            variants={pathVariants}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
         </motion.svg>
-      </div>
+      </motion.div>
 
-      
       <motion.div
         className="title"
         initial={{ y: -250 }}
         animate={{ y: -10 }}
-        transition={{ delay: 0.2, type: 'spring', stiffness: 1200 }}
+        transition={{ delay: 0.2, type: "spring", stiffness: 1200 }}
       >
         <h1>Pizza Joint</h1>
       </motion.div>
